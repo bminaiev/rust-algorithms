5034e895e2a98357b380fa2035d609a63862485e
Borys Minaiev <bminaiev@gmail.com>
2021-03-02 20:49:00 +0000

    add fenwick test

--- src/lib/seg_trees/fenwick.rs
+++ src/lib/seg_trees/fenwick.rs
@@ -1,5 +1,5 @@
 pub struct Fenwick {
-    pub values: Vec<i32>
+    values: Vec<i64>
 }
 
 impl Fenwick {
@@ -15,15 +15,47 @@ impl Fenwick {
         }
     }
 
-    fn add(&mut self, mut pos: usize, change: i32) {
+    fn add(&mut self, mut pos: usize, change: i64) {
         while pos < self.values.len() {
             self.values[pos] += change;
             pos |= pos + 1;
         }
     }
 
-    pub fn new(n: usize) -> Self {
+    pub(crate) fn new(n: usize) -> Self {
         let values = vec![0; n];
         Fenwick { values }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use rand::prelude::*;
+    use crate::lib::seg_trees::fenwick::Fenwick;
+
+    #[test]
+    fn stress() {
+        let mut rnd = StdRng::from_rng(thread_rng()).unwrap();
+        const MAX_N: usize = 100;
+        const MAX_VAL: i32 = std::i32::MAX;
+        const TESTS_N: usize = 100;
+
+        for _ in 0..TESTS_N {
+            let n: usize = rnd.gen_range(1..=MAX_N);
+            let mut fenw = Fenwick::new(n);
+            let mut slow_vec = vec![0i64; n];
+            for _ in 0..TESTS_N {
+                let pos = rnd.gen_range(0..n);
+                if rnd.gen_bool(0.5) {
+                    let sum_from_fenw = fenw.get_sum(pos);
+                    let sum_slow = slow_vec[0..=pos].iter().sum();
+                    assert_eq!(sum_from_fenw, sum_slow);
+                } else {
+                    let change = rnd.gen_range(0..MAX_VAL) as i64;
+                    fenw.add(pos, change);
+                    slow_vec[pos] += change;
+                }
+            }
+        }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,5 @@
 mod lib;
 
-use lib::seg_trees::fenwick::Fenwick;
-
-#[derive(Default)]
-struct Scanner {
-    buffer: Vec<String>,
-}
-
-// Scanner code is copied from Russell Emerine's solution
-// http://codeforces.com/contest/1477/submission/105755265
-impl Scanner {
-    fn next<T: std::str::FromStr>(&mut self) -> T {
-        loop {
-            if let Some(token) = self.buffer.pop() {
-                return token.parse().ok().expect("Failed parse");
-            }
-            let mut input = String::new();
-            std::io::stdin().read_line(&mut input).expect("Failed read");
-            self.buffer = input.split_whitespace().rev().map(String::from).collect();
-        }
-    }
-
-    #[allow(dead_code)]
-    fn next_string(&mut self) -> Vec<u8> {
-        self.next::<String>().into_bytes()
-    }
-}
-
-pub fn main() {
-    let mut sc = Scanner::default();
-    let n: usize = sc.next();
-    let f = Fenwick::new(n);
+fn main() {
+    println!("Hello");
 }
